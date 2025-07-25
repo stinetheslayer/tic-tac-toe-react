8d17ba519de4cf69537279fb5ab217f795c724d0
behrouz43salimi <143632590+stinetheslayer@users.noreply.github.com>
2025-07-25 15:54:35 +0200

    feat(player): toggle input field with edit button

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,34 @@
-export default function Player({name, symbol}) {
+import { useState } from 'react';
+
+export default function Player({ name, symbol }) {
+    const [isEditing, setIsEditing] = useState(false);
+
+    function handleEditClick() {
+        setIsEditing(true)
+    }
+
+
+    let playerName = <span className="player-name">{name}</span>
+
+    if (isEditing) {
+        playerName = <input type="text" required />
+    }
+
+
     return (
         <li>
             <span className="player">
-                <span className="player-name">{name}</span>
+                {playerName}
                 <span className="player-symbol">{symbol}</span>
-             </span>
-            <button>Edit</button>
+            </span>
+            <button onClick={handleEditClick}>Edit</button>
         </li>
     )
 }
+
+{/*{!isEditing ? (*/}
+{/*    <>*/}
+{/*<span className="player-name">{name}</span>*/}
+{/*    </>*/}
+{/*) : ( <input></input> ) }*/}
+{/*{isEditing && (<input></input>)}*/}
