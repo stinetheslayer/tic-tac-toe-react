b9a8440dd819e40380869311e9114228767f5eb6
behrouz43salimi <143632590+stinetheslayer@users.noreply.github.com>
2025-07-28 16:07:31 +0200

    fix(GameOver): make rematch button functional
    Make the rematch button functional so players can restart the game from the GameOver screen.

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ function App() {
 
     const activePlayer = deriveActivePlayer(gameTurns);
 
-    let gameBoard = initialGameBoard;
+    let gameBoard = [...initialGameBoard.map(array => [...array])];
 
     for (const turn of gameTurns) {
         const {square, player} = turn;
@@ -67,13 +67,17 @@ function App() {
         });
     }
 
+    function handleRestart() {
+        setGameTurns([])
+    }
+
     return <main>
         <div id="game-container">
             <ol id="players" className="highlight-player">
                 <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'}/>
                 <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'}/>
             </ol>
-            {(winner || hasDraw) && <GameOver winner={winner} />}
+            {(winner || hasDraw) && <GameOver winner={winner} onRestart={handleRestart} />}
             <GameBoard board={gameBoard} onSelectSquare={handleSelectSquare}/>
         </div>
         <Log turns={gameTurns}/>
