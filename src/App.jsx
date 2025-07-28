560820f60455ff631e3ba54060ee9e9f0ab0f8dd
behrouz43salimi <143632590+stinetheslayer@users.noreply.github.com>
2025-07-28 10:12:12 +0200

    feat(main game area): create GameBoard component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Player from './components/Player.jsx'
+import GameBoard from './components/GameBoard.jsx'
 
 function App() {
     return <main>
@@ -7,8 +8,7 @@ function App() {
                 <Player initialName="Player 1" symbol="X" />
                 <Player initialName="Player 2" symbol="O" />
             </ol>
-
-            GAME BOARD
+            <GameBoard />
         </div>
 
         LOG
