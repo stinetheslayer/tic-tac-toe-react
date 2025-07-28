22f9dff043fd8fb76761fb799c01085827225ed4
behrouz43salimi <143632590+stinetheslayer@users.noreply.github.com>
2025-07-28 15:19:12 +0200

    feat(game): declare winner and display message
    Added logic to check for a winning combination after each move.
    Displays a message to indicate the winner once the game is over.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,14 @@ import {useState} from 'react';
 import Player from './components/Player.jsx'
 import GameBoard from './components/GameBoard.jsx'
 import Log from './components/Log.jsx'
+import { WINNING_COMBINATIONS } from './winning-combinations.js';
+
+const initialGameBoard = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null]
+]
+
 
 function deriveActivePlayer(gameTurns) {
     let currentPlayer = 'X';
@@ -13,10 +21,36 @@ function deriveActivePlayer(gameTurns) {
     return currentPlayer;
 }
 
+
+
 function App() {
-    const [gameTurns, setGameTurns] = useState([])
+    const [gameTurns, setGameTurns] = useState([]);
 
     const activePlayer = deriveActivePlayer(gameTurns);
+
+    let gameBoard = initialGameBoard;
+
+    for (const turn of gameTurns) {
+        const {square, player} = turn;
+        const {row, col} = square;
+
+        gameBoard[row][col] = player;
+    }
+
+    let winner;
+
+    for (const combination of WINNING_COMBINATIONS) {
+        const firstSquareSymbol = gameBoard[combination[0].row][combination[0].column]
+        const secondSquareSymbol = gameBoard[combination[1].row][combination[1].column]
+        const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].column]
+
+        if (firstSquareSymbol &&
+            firstSquareSymbol === secondSquareSymbol &&
+            firstSquareSymbol === thirdSquareSymbol
+        ) {
+            winner = firstSquareSymbol;
+        }
+    }
 
     function handleSelectSquare(rowIndex, colIndex) {
         setGameTurns((prevTurns) => {
@@ -34,10 +68,10 @@ function App() {
                 <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'}/>
                 <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'}/>
             </ol>
-            <GameBoard turns={gameTurns} onSelectSquare={handleSelectSquare} />
+            {winner && <p> You won, {winner}!</p>}
+            <GameBoard board={gameBoard} onSelectSquare={handleSelectSquare}/>
         </div>
-
-        <Log turns={gameTurns} />
+        <Log turns={gameTurns}/>
     </main>;
 }
 
